8efd5a7e0a18cdbb4969ed58b1bc42c5ee3ed557
Stephen Rudolph <Stephen.Rudolph@gmail.com>
2021-09-29 11:57:26 -0500

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,15 +3,57 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+
+const Wrapper = styled.div`
+  width: fit-content;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  padding: 12px 16px 12px 16px;
+  white-space: nowrap;
+  display: block;
+
+  :focus {
+    border: 2px solid Highlight;
+    padding: 10px 14px 10px 14px;
+  }
+
+  :hover {
+    color: ${COLORS.black};
+  }
+
+  select {
+    box-sizing: border-box;
+    font-family: inherit;
+    font-size: 16px;
+    -webkit-appearance: none;
+    appearance: none;
+    display: inline-block;
+    background: none;
+    color: inherit;
+    border: none;
+    padding-right: 24px;
+
+    :hover, :focus {
+      outline: none;
+    }
+  }
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper role="combobox" aria-label={label}>
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+      <IconWrapper>
+        <Icon id="chevron-down" strokeWidth="2" size="12" />
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
